339b9ed9bcf011b7e4f827d44e20b8fed65d12d1
oliwier <oliwierjaworski01@outlook.com>
2025-06-18 23:13:13 +0200

    starter setup with treesitter plugin

---
+++ lua/options.lua
@@ -0,0 +1,22 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+        
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.wrap = false --text wrapup instead of continious offscreen
+
+vim.opt.expandtab = true 
+vim.opt.tabstop = 4 
+vim.opt.shiftwidth = 4
+
+vim.opt.clipboard = "unnamedplus" --cpy newest from clipboard
+vim.opt.scrolloff = 999 -- after x interval numbers in middle
+
+vim.opt.virtualedit = "block"
+
+vim.opt.inccommand = "split" --shows what will be changed at the bottom instead of not showing
+vim.opt.ignorecase = true
+
+vim.opt.termguicolors = true
+
