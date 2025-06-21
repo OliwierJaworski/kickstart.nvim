7f27c0003c1cccbd5b1fcadb12bc8c71a9c8d533
oliwier <oliwierjaworski01@outlook.com>
2025-06-22 01:01:15 +0200

    fixed alpha added autopairs

--- lua/config/plugins/alpha.lua
+++ lua/config/plugins/alpha.lua
@@ -13,67 +13,20 @@ return {
         local color5 = vim.g.color5 or "#ffffff"
         local color6 = vim.g.color6 or "#ffffff"
 
-        -- helper function for utf8 chars
-        local function getCharLen(s, pos)
-            local byte = string.byte(s, pos)
-            if not byte then
-                return nil
-            end
-            return (byte < 0x80 and 1) or (byte < 0xE0 and 2) or (byte < 0xF0 and 3) or (byte < 0xF8 and 4) or 1
-        end
+        local alpha = require("alpha")
+        local dashboard = require("alpha.themes.dashboard")
 
-        local function applyColors(logo, colors, logoColors)
-            dashboard.section.header.val = logo
+        dashboard.section.header.val = {
+            "                                                     ",
+            "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+            "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+            "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+            "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+            "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+            "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+            "                                                     ",
+        }
 
-            for key, color in pairs(colors) do
-                local name = "Alpha" .. key
-                vim.api.nvim_set_hl(0, name, color)
-                colors[key] = name
-            end
-
-            dashboard.section.header.opts.hl = {}
-            for i, line in ipairs(logoColors) do
-                local highlights = {}
-                local pos = 0
-
-                for j = 1, #line do
-                    local opos = pos
-                    pos = pos + getCharLen(logo[i], opos + 1)
-
-                    local color_name = colors[line:sub(j, j)]
-                    if color_name then
-                        table.insert(highlights, { color_name, opos, pos })
-                    end
-                end
-
-                table.insert(dashboard.section.header.opts.hl, highlights)
-            end
-            return dashboard.opts
-        end
-
-        require("alpha").setup(applyColors({
-            [[███████╗    ██████╗ ]],
-            [[██╔════╝    ██╔═══╝ ]],
-            [[█████╗      ██████╗ ]],
-            [[██╔══╝      ██╔═══╝ ]],
-            [[███████╗    ██║     ]],
-            [[╚══════╝    ╚═╝     ]],
-            [[N  E  O  V  I  M    ]],
-        }, {
-                ["a"] = { fg = color9, ctermfg = 33},
-                ["b"] = { fg = color3, ctermfg = 33},
-                ["c"] = { fg = color4, ctermfg = 33},
-                ["d"] = { fg = color5, ctermfg = 33},
-                ["e"] = { fg = color6, ctermfg = 33},
-            }, {
-                [[bbbbbbba    cccccca ]],
-                [[bbaaaaaa    ccaaaaa ]],
-                [[bbbbba      cccccca ]],
-                [[bbaaaa      ccaaaaa ]],
-                [[bbbbbbba    cca     ]],
-                [[aaaaaaaa    aaa     ]],
-                [[d  d  d  e  e  e    ]],
-            }))
         dashboard.section.buttons.val = {
             dashboard.button( "e", "  > New file" , ":ene <BAR> startinsert <CR>"),
             dashboard.button( "f", "󰱼  > Find file", ":lua require('telescope.builtin').find_files({ find_command = { 'rg', '--files' } })<CR>"),
@@ -90,6 +43,13 @@ return {
             "",
             "Welcome!",
         }
+        -- Send config to alpha
+        alpha.setup(dashboard.opts)
+
+        -- Disable folding on alpha buffer
+        vim.cmd([[
+        autocmd FileType alpha setlocal nofoldenable
+        ]])
 
     end,
 }
